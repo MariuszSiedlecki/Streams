f0efb641a0e9043b09c68cbbde05b64fcc46c555
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-01-31 22:19:42 +0100

    first

---
+++ src/main/java/MainPerson.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainPerson {
+    public static void main(String[] args) {
+
+        List<Person>people = new ArrayList<Person>();
+        people.add(new Person("Mariusz", "Nowak",38));
+        people.add(new Person("Jan","Bogaty",52));
+        people.add(new Person("Przemysław","Zgodny",15));
+        people.add(new Person("Adam","Kropka",10));
+
+        people.stream()
+                .peek(s->{
+                    s.setNamme(s.getNamme().substring(0,1));
+                    s.setSurname(s.getSurname().substring(s.getSurname().length()-1));
+                })
+                .reduce((s,s1)->{
+                    s.setNamme(s.getNamme()+s1.getNamme());
+                    s.setSurname(s.getSurname() + s1.getSurname());
+                    s.setAge(s.getAge()+ s1.getAge());
+                    return s;
+                })
+                    .ifPresent(s-> System.out.println(s));
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,10 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+   private String namme;
+   private String Surname;
+   private int age;
+}
