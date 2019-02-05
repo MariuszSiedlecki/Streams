a2c09d0efeffea7598e23c8badfe3585c6fb4332
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-02-05 09:50:52 +0100

    chang7

---
+++ src/main/java/task5/MainSumNameSurnameIsSame.java
@@ -0,0 +1,25 @@
+package task5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MainSumNameSurnameIsSame {
+    public static void main(String[] args) {
+
+
+    List<Person> people = new ArrayList<>();
+    people.add(new Person("Jan", "Trzy",33));
+    people.add(new Person("Ola","Tocc",25));
+    people.add(new Person("Maks","Cztery",9));
+    people.add(new Person("Ania","Znana",25));
+    people.add(new Person("Paweł","Sum",27));
+    people.add(new Person("Mariusz","To",35));
+
+    Map<Integer,List<Person>> listPeople = people.stream()
+            .collect(Collectors.groupingBy(s->s.getName().length()+ s.getSurname().length()));
+        System.out.println("Groups of people according to long names and surnames:" + listPeople);
+
+}
+}
