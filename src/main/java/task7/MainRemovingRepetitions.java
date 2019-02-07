17cece391f5d3a05cc5e4ca5142eb69a4b69139b
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-02-07 22:43:33 +0100

    change9

---
+++ src/main/java/task7/MainRemovingRepetitions.java
@@ -0,0 +1,18 @@
+package task7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainRemovingRepetitions {
+    public static void main(String[] args) {
+
+        List<Person>people = new ArrayList<>();
+        people.add(new Person("Jan", "Niepowtorzony",33));
+        people.add(new Person("Anna","Powtorzona",25));
+        people.add(new Person("Olo","Jedyna",25));
+        people.add(new Person("Anna","Powtorzona",25));
+        people.add(new Person("Paweł","Oryginał",66));
+        people.add(new Person("Anna","Powtorzona",25));
+
+    }
+}
---
+++ src/main/java/task7/Person.java
@@ -0,0 +1,12 @@
+package task7;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private String surName;
+    private  int age;
+}
